42c31bf0c9e52262d54e2e779638d8118004f6cb
JCSnap <e0970167@u.nus.edu>
2023-08-15 18:52:38 +0800

    added loops in c

---
+++ Loops.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Display different types of loops
+int main(void) {
+  int sum = 0, i = 1;
+  while (i <= 10) {
+    sum += i;
+    i++;
+    printf("Using while loop: %d\n", sum);
+  }
+
+  sum = 0;
+  i = 1;
+  do {
+    sum += i;
+    i++;
+    printf("Using do-while loop: %d\n", sum);
+  } while (i <= 10);
+
+  for (sum = 0, i = 1; i <= 10; i++) {
+    sum += i;
+    printf("Using for loop: %d\n", sum);
+  }
+}
+
