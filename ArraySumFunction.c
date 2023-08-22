3820797b192e46cf92a6b9482a119077ba37197b
JCSnap <e0970167@u.nus.edu>
2023-08-22 16:59:36 +0800

    added comments for arraysumfunction

--- ArraySumFunction.c
+++ ArraySumFunction.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int sum(int [], int);
+// we can also write it as
+// int sum(int *, int);
+// because the array name is a pointer to the first element of the array
 
 int main(void) {
   int val[5] = {1, 2, 3, 4, 5};
@@ -16,3 +19,8 @@ int sum(int arr[], int size) {
   }
   return sum;
 }
+// We can also write it as
+// int sum(int *arr, int size)
+// because the array name is a pointer to the first element of the array
+// This is called a syntax sugar
+// It's just a different way of writing the same thing
