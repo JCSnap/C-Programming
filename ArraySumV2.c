683cd201a985a474ac9bb0a09fa3f34c0ba0c2da
JCSnap <e0970167@u.nus.edu>
2023-08-22 16:43:11 +0800

    added comment to array

--- ArraySumV2.c
+++ ArraySumV2.c
@@ -3,6 +3,8 @@
 
 int main(void) {
   int numbers[MAX] = {1, 2, 3, 4, 5};
+  int numbers2[] = {1, 2, 3, 4, 5}; // C can infer size
+  int numbers3[10] = {1, 2, 3, 4, 5}; // If not initialized, C will set to 0
   int i, sum = 0;
 
   for (i = 0; i < MAX; i++) {
